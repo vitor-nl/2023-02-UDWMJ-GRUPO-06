c7755d13bdd832736756c8788ffa90f7758f38ca
Igor Viana Tomaz <igorvianat@gmail.com>
2023-11-16 19:57:51 -0300

    zerando repositório

--- cliente_redesocial.py
+++
@@ -1,6 +0,0 @@
-import redesocial, cliente_class
-
-teste = redesocial.RedeSocial("Piratex", "Teste")
-teste_cliente = cliente_class.Cliente("Cleito", "Faca", "Rua Banana, 666", "(51)9999-4444", "cleitocorterapido@gmail.com", "Não definido")
-
-
