eab25df0e92878c257f170126eee425446a211a5
GuiAR404 <guilherme.ascenco100@gmail.com>
2023-12-04 05:32:17 -0300

    Order e OrderItem 100% funcional

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -6,9 +6,8 @@ app_name = 'orders'
 
 urlpatterns = [
     path('', views.list_orders, name='list_orders'),
-    path('', views.list_orders, name='list_orders'),
+    path('list/', views.list_orders, name='list_orders'),
     path('adicionar/', views.add_order, name='add_order'),
     path('editar/<int:id_order>/', views.edit_order, name='edit_order'),
     path('excluir/<int:id_order>/', views.delete_order, name='delete_order'),
 ]
-
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -2,46 +2,98 @@ from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework import viewsets
 from .serializer import OrderSerializer, OrderItemSerializer
 from .models import Order, OrderItem
+from .forms import OrderForm, OrderItemForm
 
 # Create your views here.
 
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer  
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
 def add_order(request):
-    template_name = 'order/add_order.html'
+    template_name = 'orders/add_order.html'
     context = {}
     if request.method == 'POST':
-        form = orderForm(request.POST)
+        form = OrderForm(request.POST, request.FILES)
         if form.is_valid():
             f = form.save(commit=False)
             f.save()
             form.save_m2m()
             return redirect('orders:list_orders')
-    form = orderForm()
+    form = OrderForm()
     context['form'] = form
     return render(request, template_name, context)
+
 
 def list_orders(request):
     template_name = 'orders/list_orders.html'
     orders = Order.objects.filter()
     context = {
-        'Orders': orders
+        'orders': orders
     }
     return render(request, template_name, context)
 
 def edit_order(request, id_order):
-    template_name = 'order/add_order.html'
-    context ={}
-    order = get_object_or_404(Order, id=id_order)
-    if request.method == 'POST':
-        form = orderForm(request.POST, instance=order)
+    template_name = 'orders/add_order.html'
+    context = {}
+    order = get_object_or_404(Order, id =id_order)
+    if request.method =='POST':
+        form = OrderForm(request.POST, instance = order)
         if form.is_valid():
             form.save()
-            return redirect('categories:list_categories')
-    form = orderForm(instance=order)
+            return redirect('orders:list_orders')
+    form = OrderForm(instance=order)
     context['form'] = form
     return render(request, template_name, context)
 
 def delete_order(request, id_order):
-    order = order.objects.get(id=id_order)
+    order = Order.objects.get(id=id_order)
     order.delete()
-    return redirect('categories:list_categories')
+    return redirect('orders:list_orders')
 
+#=======================================================================================================================================================================
+
+def add_orderItem(request):
+    template_name = 'orders/add_orderItem.html'
+    context = {}
+    if request.method == 'POST':
+        form = OrderItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('/pedidos_itens/')
+    form = OrderItemForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+def list_orderItens(request):
+    template_name = 'orders/list_orderItens.html'
+    orderItems = OrderItem.objects.all()  
+    context = {
+        'orderItems': orderItems  
+    }
+    return render(request, template_name, context)
+
+def edit_orderItem(request, id_orderItem):
+    template_name = 'orders/add_orderItem.html'
+    context = {}
+    orderItem = get_object_or_404(OrderItem, id=id_orderItem) 
+    if request.method == 'POST':
+        form = OrderItemForm(request.POST, instance=orderItem) 
+        if form.is_valid():
+            form.save()
+            return redirect('/pedidos_itens/')
+    form = OrderItemForm(instance=orderItem)
+    context['form'] = form
+    return render(request, template_name, context)
+
+def delete_orderItem(request, id_orderItem):
+    orderItem = OrderItem.objects.get(id=id_orderItem)
+    orderItem.delete()
+    return redirect('/pedidos_itens/')
