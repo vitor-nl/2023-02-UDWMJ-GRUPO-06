8ea3339169aa084aa25bd22fc0abd7f8390b21ff
KathlennPadilha <kathlennpadilha@gmail.com>
2023-12-04 20:22:34 -0300

    ad gênero

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -4,14 +4,11 @@ from rest_framework import routers
 
 app_name = 'clients'
 
-router = routers.DefaultRouter()
-router.register('clientes', views.ClientViewSet, basename='clientes')
-router.register('clientes_redessociais', views.ClientSocialnetworkViewSet, basename='clientes_redessociais')
 
 urlpatterns = [
-    path('', views.list_client, name='list_client'),
+    path('', views.list_client, name='list_clients'),
     path('adicionar/', views.add_client, name='add_client'),
     path('editar/<int:id_client>/', views.edit_client, name='edit_client'),
-    path('excluir/<int:id_client>/', views.delete_client, name='delete_client'),
+    path('clientes/excluir/<int:id_client>/', views.delete_clients, name='delete_clients'),
 
 ]
